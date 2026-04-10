be6f12be1d260ef8191800d5c2dc702fbe1c730d
wenkanglucky-gif <wenkanglucky@gmail.com>
2026-04-10 11:46:54 -0400

    Delete outdated app folder

--- app/src/main/java/com/example/shareat/ChatMessage.kt
+++
@@ -1,9 +0,0 @@
-package com.example.shareat
-
-data class ChatMessage(
-    val message_id: String = "",
-    val sender_id: String = "",
-    val sender_name: String = "",
-    val text: String = "",
-    val sent_at: Long = 0L
-)
--- app/src/main/java/com/example/shareat/NearbyFoodPost.kt
+++
@@ -1,6 +0,0 @@
-package com.example.shareat
-
-data class NearbyFoodPost(
-    val post: FoodPost,
-    val distanceKm: Double
-)
